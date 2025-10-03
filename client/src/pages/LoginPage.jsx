c0b171cbb3f31452dd1ea49920516cab555d15bf
Rafael <ansdles@live.com>
2025-10-02 21:01:05 -0300

    fix: adiciona credentials a todas as chamadas fetch SEGUNDA TENTATIVA

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -19,6 +19,7 @@ function RegisterModal({ onClose }) {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
+        credentials: 'include'
       });
       const data = await response.json();
       setMessage(data.message); //Exibe a mensagem de sucesso ou erro
